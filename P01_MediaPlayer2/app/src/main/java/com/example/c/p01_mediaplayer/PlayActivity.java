14f82fadcd9df1ee60ae7a9e91204d318aaf19b3
= <ylh6151@gmail.com>
2015-08-01 13:26:06 +0900

    p01

--- P01_MediaPlayer2/app/src/main/java/com/example/c/p01_mediaplayer/PlayActivity.java
+++ P01_MediaPlayer2/app/src/main/java/com/example/c/p01_mediaplayer/PlayActivity.java
@@ -1,5 +1,6 @@
-package com.example.c.t11_mediaplayer;
+package com.example.c.p01_mediaplayer;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
@@ -10,18 +11,29 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 
 
-public class MainActivity extends ActionBarActivity {
+public class PlayActivity extends ActionBarActivity {
 
     MediaPlayer mp = null;
     ProgressBar progressBar;
+    String strName = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_play);
+
+        Intent intent = getIntent();
+        strName =  intent.getExtras().get("name").toString();
+
+        Log.d("P01 playactivity", "listview id : " + strName );
+        TextView title = (TextView) findViewById(R.id.textView);
+        title.setText(strName);
 
         Button btnPlay = (Button)findViewById(R.id.btnPlay);
         Button btnStop = (Button)findViewById(R.id.btnStop);
@@ -33,23 +45,20 @@ public class MainActivity extends ActionBarActivity {
             @Override
             public void run() {
                 while(true){
-                    if (mp != null) {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
-                        if(mp.isPlaying()) {
+                    if(mp != null && mp.isPlaying()) {
 
-                            //float progress = (float)mp.getCurrentPosition() / (float)mp.getDuration();
-                            //progressBar.setProgress((int){progress * 100});
+                        //float progress = (float)mp.getCurrentPosition() / (float)mp.getDuration();
+                        //progressBar.setProgress((int){progress * 100});
 
-                            //start 버튼 클릭시 setMax 하고..
-                            Log.d("Mediaplayer", "mp.getCurrentPosition() : " + mp.getCurrentPosition());
-                            Log.d("Mediaplayer", "mp.getDuration() : " + mp.getDuration());
-                            progressBar.setProgress(mp.getCurrentPosition());
-                        }
+                        Log.d("Mediaplayer", "mp.getCurrentPosition() : " + mp.getCurrentPosition());
+                        Log.d("Mediaplayer", "mp.getDuration() : " + mp.getDuration());
+                        progressBar.setProgress(mp.getCurrentPosition());
                     }
                 }
             }
@@ -58,12 +67,12 @@ public class MainActivity extends ActionBarActivity {
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String path = Environment.getExternalStorageDirectory().toString();
-                path += "/Music/frozen/05 - Let It Go.mp3";
+                String path = Environment.getExternalStorageDirectory().toString(); // /storage/emulated/0
+                path += "/Music/frozen/" + strName;
 
-                if (mp == null) {
+//                if (mp == null) {
                     mp = new MediaPlayer();
-                }
+//                }
 
                 try {
                     mp.setDataSource(path);
@@ -89,7 +98,7 @@ public class MainActivity extends ActionBarActivity {
             }
         });
 
-        btnPause.setOnClickListener(new View.OnClickListener() {
+/*        btnPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mp != null) {
@@ -97,12 +106,13 @@ public class MainActivity extends ActionBarActivity {
                 }
             }
         });
+        */
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_play, menu);
         return true;
     }
 
