d61244cdf58d49fe359cedb7c1ead53a585360a3
mj <ylh6151@gmail.com>
2015-08-02 17:37:48 +0900

    progressbar dialog

--- T18_AlertDialog/app/src/main/java/com/example/c/t18_alertdialog/MainActivity.java
+++ T18_AlertDialog/app/src/main/java/com/example/c/t18_alertdialog/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.c.t18_alertdialog;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBarActivity;
@@ -11,18 +12,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private static final int SHOW_DIALOG = 1;
+    private static final int ALERT_DIALOG = 1;
+    private static final int CUSTOM_DIALOG = 2;
 
     @Override
     protected Dialog onCreateDialog(int id) {
 
         switch(id){
-            case SHOW_DIALOG:
+            case ALERT_DIALOG:
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setTitle("종료 확인");
                 builder.setMessage("앱을 종료하시겠습니까?");
@@ -41,6 +44,22 @@ public class MainActivity extends ActionBarActivity {
 
                 AlertDialog dialog = builder.create();
                 return dialog;
+            case CUSTOM_DIALOG:
+                Dialog customDialog = new Dialog(this);
+                customDialog.setContentView(R.layout.custom_dialog);
+
+                TextView customTextView = (TextView) customDialog.findViewById(R.id.customTextView);
+                customTextView.setText("customDialog - onCreated");
+
+                Button customBtn = (Button) customDialog.findViewById(R.id.button);
+                customBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "customDialog button", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                return customDialog;
         }
         return super.onCreateDialog(id);
     }
@@ -54,7 +73,68 @@ public class MainActivity extends ActionBarActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialog(SHOW_DIALOG);
+                showDialog(ALERT_DIALOG);
+            }
+        });
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog(CUSTOM_DIALOG);
+            }
+        });
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog progressDialog = ProgressDialog.show(MainActivity.this,"Title", "Now Loading....", true, false);
+                //indeterminate : true 이면 back 키 눌러도 반응 없음.
+                //cancelable : false 이면 dialog 외 빈공간 눌러도 반응 없음.
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        progressDialog.dismiss();
+                    }
+                }).start();
+            }
+        });
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog progressDialog2 =new ProgressDialog(MainActivity.this);
+                progressDialog2.setMessage("Now Loading.....");
+                progressDialog2.setCancelable(false);
+                progressDialog2.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                progressDialog2.setProgress(0);
+                progressDialog2.show();
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        int progress = 0;
+                        while(progress < 100){
+                            progress ++;
+                            progressDialog2.setProgress(progress);
+
+                            try {
+                                Thread.sleep(100);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        progressDialog2.dismiss();
+                    }
+                }).start();
             }
         });
     }
