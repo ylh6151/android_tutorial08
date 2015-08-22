b6c983ed93fbf20f6631e8060889c598f1b819de
mj <ylh6151@gmail.com>
2015-08-22 14:06:09 +0900

    webview

--- T26_WebView/app/src/main/java/com/example/c/t26_webview/MainActivity.java
+++ T26_WebView/app/src/main/java/com/example/c/t26_webview/MainActivity.java
@@ -2,16 +2,60 @@ package com.example.c.t26_webview;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar progressBar;
+
+    WebChromeClient webChromeClient = new WebChromeClient(){
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            super.onProgressChanged(view, newProgress);
+
+            if(newProgress == 100)
+                progressBar.setVisibility(View.INVISIBLE);
+            else{
+                progressBar.setVisibility(View.VISIBLE);
+                progressBar.setProgress(newProgress);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        //setWebChromeClient : 웹브라우저 사용자 인터페이스에 영향을 주는 이벤트 cache, cookie, map 등 ,
+        //setWebViewClient: 화면을 그리는데 영향을 주는 이벤트
+        //webView.loadUrl("http://www.daum.net");
+        webView.loadUrl("file:///android_asset/hello.html");
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        webView.setWebChromeClient(webChromeClient);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        if(webView.canGoBack())
+            webView.goBack();
+        else
+            finish();
     }
 
     @Override
